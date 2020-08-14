68539a6932c5a61e2c2049fb45e8304e37f4608a
mithun <mithunpaul08@gmail.com>
2020-08-14 16:30:35 -0700

    didnt run on hpc yet. just took some time to verify locally by tracing the control flow. looks ok, the new change of warmup bug fix seems to work on laptop. lets try again for 1 epoch

--- mithun_scripts/run_glue.sh
+++ mithun_scripts/run_glue.sh
@@ -37,6 +37,9 @@ echo "data_dir is $DATA_DIR"
 
 mkdir -p OUTPUT_DIR
 
+#for laptop run from terminal
+echo $args
+
 
 if [ $MACHINE_TO_RUN_ON == "server" ]; then
        python3.6 ../examples/text-classification/run_glue.py $args
@@ -46,8 +49,7 @@ fi
 
 
 
-#for laptop run from terminal
-echo $args
+
 
 #python3 ../examples/text-classification/run_glue.py $args
 
@@ -61,7 +63,7 @@ echo $args
 #to load a trained model
 #python3.6 ../examples/text-classification/run_glue.py --model_name_or_path "output/checkpoint-37000/" --tokenizer_name  bert-base-uncased    --task_name fevercrossdomain   --do_predict     --data_dir "../src/transformers/data/datasets/fever/fevercrossdomain/lex"     --max_seq_length 128      --per_device_eval_batch_size=8       --per_device_train_batch_size=8        --learning_rate 2e-5      --num_train_epochs 1.0      --output_dir "./output" --overwrite_output_dir --save_steps 14000 --weight_decay 0.01 --adam_epsilon 1e-6
 #cross domain lex bert cased
-#--model_name_or_path bert-base-cased --task_name fevercrossdomain --do_train --do_eval --do_predict --data_dir /Users/mordor/research/transformers/src/transformers/data/datasets/fever/fevercrossdomain/lex/figerspecific --max_seq_length 128 --per_device_eval_batch_size=16 --per_device_train_batch_size=16 --learning_rate 1e-5 --num_train_epochs 1.0 --output_dir output/ --overwrite_output_dir --save_steps 74000 --weight_decay 0.01 --adam_epsilon 1e-6 --overwrite_cache
+#--model_name_or_path bert-base-cased --task_name fevercrossdomain --do_train --do_eval --do_predict --data_dir /Users/mordor/research/huggingface/src/transformers/data/datasets/fever/fevercrossdomain/lex/figerspecific --max_seq_length 128 --per_device_eval_batch_size=16 --per_device_train_batch_size=16 --learning_rate 1e-5 --num_train_epochs 1 --output_dir output/fever/fevercrossdomain/lex/figerspecific/bert-base-cased/128/1/ --overwrite_output_dir --weight_decay 0.01 --adam_epsilon 1e-6 --overwrite_cache
 
 #cross domain delex bert cased
 #--model_name_or_path bert-base-cased --task_name fevercrossdomain --do_train --do_eval --do_predict --data_dir /Users/mordor/research/transformers/src/transformers/data/datasets/fever/fevercrossdomain/delex/figerspecific --max_seq_length 128 --per_device_eval_batch_size=16 --per_device_train_batch_size=16 --learning_rate 1e-5 --num_train_epochs 1.0 --output_dir output/ --overwrite_output_dir --save_steps 74000 --weight_decay 0.01 --adam_epsilon 1e-6 --overwrite_cache
