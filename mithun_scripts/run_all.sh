mithunpaul08/transformers

commit c86addfd50c88d3bf1fdc4f35220f0f006a19948

previous commit gave 51.88 fnc score for 1st epoch of delex cased. but that is not matching with any other numbers i have seen so far. will run again for 1 epoch of lex cased, just to confirm.

mithun_scripts/run_all.sh
@@ -43,7 +43,7 @@ echo $EPOCHS
 
 export DATASET="fever"
 export basedir="$DATA_DIR_BASE/$DATASET"
-export TASK_TYPE="delex" #options for task type include lex,delex,and combined"". combined is used in case of student teacher architecture which will load a paralleldataset from both lex and delex folders
+export TASK_TYPE="lex" #options for task type include lex,delex,and combined"". combined is used in case of student teacher architecture which will load a paralleldataset from both lex and delex folders
 export SUB_TASK_TYPE="figerspecific" #options for TASK_SUB_TYPE (usually used only for delex)  include [oa, figerspecific, figerabstract, oass, simplener]
 export TASK_NAME="fevercrossdomain" #options for TASK_NAME  include fevercrossdomain,feverindomain,fnccrossdomain,fncindomain
 export DATA_DIR="$DATA_DIR_BASE/$DATASET/$TASK_NAME/$TASK_TYPE/$SUB_TASK_TYPE"

mithun_scripts/run_on_hpc_ocelote_venv_array.sh
@@ -3,7 +3,7 @@
 #PBS -q standard
 #PBS -l select=1:ncpus=28:mem=168gb:pcmem=6gb:ngpus=1:os7=True
 ### Specify a name for the job
-#PBS -N delex_bert_cased_1epochs
+#PBS -N lex_bert_cased_1epochs
 ### Specify the group name
 #PBS -W group_list=msurdeanu
 ### Used if job requires partial node only

src/transformers/tokenization_bert.py
@@ -34,9 +34,9 @@ PRETRAINED_VOCAB_FILES_MAP = {
     "vocab_file": {
         #for uofa's student teacher model we use a vocab file built on top of bert-base-uncased-vocab.txt, but has mappings for delex tokens like PERSON-C1
         #"bert-base-uncased": "https://storage.googleapis.com/fact_verification_mithun_files/bert_related/bert_base_uncased/vocab.txt",
-        "bert-base-cased": "https://storage.googleapis.com/fact_verification_mithun_files/bert_related/bert_base_cased_with_c1_fix/bert-base-cased_with_bothUppercaseC1andhashC1.txt",
+        #"bert-base-cased": "https://storage.googleapis.com/fact_verification_mithun_files/bert_related/bert_base_cased_with_c1_fix/bert-base-cased_with_bothUppercaseC1andhashC1.txt",
         #"bert-base-uncased": "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-uncased-vocab.txt",
-        #"bert-base-cased": "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-cased-vocab.txt",
+        "bert-base-cased": "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-cased-vocab.txt",
 
 
         "bert-large-uncased": "https://s3.amazonaws.com/models.huggingface.co/bert/bert-large-uncased-vocab.txt",

commit a15cf6acf8079f4e6a6cf508bd4a0290482bdfb5

all commits after c86addfd50c88d3bf1fdc4f35220f0f006a19948 introduced some bug. reverting back to parent of c86addfd50c88d3bf1fdc4f35220f0f006a19948 i.e 090391a9a02a2572bb2af3346f9c75be8c76e9e7

mithun_scripts/run_all.sh
@@ -42,7 +42,7 @@ echo "EPOCHS=$EPOCHS"
 
 export DATASET="fever"
 export basedir="$DATA_DIR_BASE/$DATASET"
-export TASK_TYPE="lex" #options for task type include lex,delex,and combined"". combined is used in case of student teacher architecture which will load a paralleldataset from both lex and delex folders
+export TASK_TYPE="delex" #options for task type include lex,delex,and combined"". combined is used in case of student teacher architecture which will load a paralleldataset from both lex and delex folders
 export SUB_TASK_TYPE="figerspecific" #options for TASK_SUB_TYPE (usually used only for delex)  include [oa, figerspecific, figerabstract, oass, simplener]
 export TASK_NAME="fevercrossdomain" #options for TASK_NAME  include fevercrossdomain,feverindomain,fnccrossdomain,fncindomain
 export DATA_DIR="$DATA_DIR_BASE/$DATASET/$TASK_NAME/$TASK_TYPE/$SUB_TASK_TYPE"

mithun_scripts/run_on_hpc_ocelote_venv_array.sh
@@ -3,8 +3,8 @@
 #PBS -q standard
 #PBS -l select=1:ncpus=28:mem=168gb:pcmem=6gb:ngpus=1:os7=True
 ### Specify a name for the job
-#PBS -N dev_branch_lex_cased_figers_128
-### Specify the group name
+#PBS -N delex_bert_cased_1epochs
+    ### Specify the group name
 #PBS -W group_list=msurdeanu
 ### Used if job requires partial node only
 #PBS -l place=pack:shared

src/transformers/tokenization_bert.py
@@ -34,9 +34,9 @@ PRETRAINED_VOCAB_FILES_MAP = {
     "vocab_file": {
         #for uofa's student teacher model we use a vocab file built on top of bert-base-uncased-vocab.txt, but has mappings for delex tokens like PERSON-C1
         #"bert-base-uncased": "https://storage.googleapis.com/fact_verification_mithun_files/bert_related/bert_base_uncased/vocab.txt",
-        #"bert-base-cased": "https://storage.googleapis.com/fact_verification_mithun_files/bert_related/bert_base_cased_with_c1_fix/bert-base-cased_with_bothUppercaseC1andhashC1.txt",
+        "bert-base-cased": "https://storage.googleapis.com/fact_verification_mithun_files/bert_related/bert_base_cased_with_c1_fix/bert-base-cased_with_bothUppercaseC1andhashC1.txt",
         #"bert-base-uncased": "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-uncased-vocab.txt",
-        "bert-base-cased": "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-cased-vocab.txt",
+        #"bert-base-cased": "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-cased-vocab.txt",
 
 
         "bert-large-uncased": "https://s3.amazonaws.com/models.huggingface.co/bert/bert-large-uncased-vocab.txt",