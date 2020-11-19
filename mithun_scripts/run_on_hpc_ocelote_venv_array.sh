b29119c3b5b756e477fe639f2ea5fafeb1abb8e6
mithun <mithunpaul08@gmail.com>
2020-11-19 00:32:04 -0700

    will run again huggingface_bert_fnc_to_fever_lex_from_stand_alone_code with seed=98283 and lr=1e-5

--- mithun_scripts/run_on_hpc_ocelote_venv_array.sh
+++ mithun_scripts/run_on_hpc_ocelote_venv_array.sh
@@ -3,7 +3,7 @@
 #PBS -q standard
 #PBS -l select=1:ncpus=28:mem=168gb:pcmem=6gb:ngpus=1:os7=True
 ### Specify a name for the job
-#PBS -N huggingface_bert_fnc_to_fever_lex_from_inside_student_teacher
+#PBS -N huggingface_bert_fnc_to_fever_lex_from_stand_alone_code
 ### Specify the group name
 #PBS -W group_list=msurdeanu
 ### Used if job requires partial node only
