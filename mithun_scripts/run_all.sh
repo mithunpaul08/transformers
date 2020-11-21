bfe506a566ff9b2ee71ec427797c68ef0952a001
mithun <mithunpaul08@gmail.com>
2020-11-20 23:42:10 -0700

    huggingface_bert_fnc_to_fever_combined_attndropout0pt9classLossWeight0pt0875

--- mithun_scripts/run_all.sh
+++ mithun_scripts/run_all.sh
@@ -128,7 +128,7 @@ export args="--model_name_or_path $BERT_MODEL_NAME   --task_name $TASK_NAME     
 --learning_rate 1e-5      --num_train_epochs $EPOCHS     --output_dir $OUTPUT_DIR --overwrite_output_dir  \
 --weight_decay 0.01 --adam_epsilon 1e-6  --evaluate_during_training \
 --task_type $TASK_TYPE --subtask_type $SUB_TASK_TYPE --machine_to_run_on $MACHINE_TO_RUN_ON \
---toy_data_dir_path $TOY_DATA_DIR_PATH --overwrite_cache --attention_dropout 0.25"
+--toy_data_dir_path $TOY_DATA_DIR_PATH --overwrite_cache --attention_dropout 0.9"
 
 
 
